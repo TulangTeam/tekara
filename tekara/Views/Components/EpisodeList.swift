bbb3947686f6d7a341878781acc4af327e2a7723
dimasdaffa <dimas.de2004@gmail.com>
2026-07-16 21:29:58 +0700

    feat: update chapterbubble and episodebubble assets, add EpisodeCard and EpisodeList components

---
+++ tekara/Views/Components/EpisodeList.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeList.swift
+//  tekara
+//
+//  Created by DIMAS DAFFA ERNANDA on 16/07/26.
+//
+
+import SwiftUI
+
+struct EpisodeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeList()
+}
