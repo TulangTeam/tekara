65eb4c64e7ab6e39d060ec52d98c914fb942ad40
dimasdaffa <dimas.de2004@gmail.com>
2026-07-18 17:07:22 +0700

    feat: add DialogueCard component with initial implementation

---
+++ tekara/Views/Components/DialogueCard.swift
@@ -0,0 +1,18 @@
+//
+//  DialogueCard.swift
+//  tekara
+//
+//  Created by DIMAS DAFFA ERNANDA on 18/07/26.
+//
+
+import SwiftUI
+
+struct DialogueCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DialogueCard()
+}
