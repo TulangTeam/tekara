94e241b63d0c5963ce25fc5462de83b2c5a34ac8
dimasdaffa <dimas.de2004@gmail.com>
2026-07-21 13:45:37 +0700

    feat: update image filenames and add SectionTitleBanner component

---
+++ tekara/Views/Components/SectionTitleBanner.swift
@@ -0,0 +1,18 @@
+//
+//  SectionTitleBanner.swift
+//  tekara
+//
+//  Created by DIMAS DAFFA ERNANDA on 21/07/26.
+//
+
+import SwiftUI
+
+struct SectionTitleBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionTitleBanner()
+}
