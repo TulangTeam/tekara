bc11dfd29c138b5724f04728f2a271f610fb3970
dimasdaffa <dimas.de2004@gmail.com>
2026-07-23 23:58:38 +0700

    feat: add ChapterHintBubble component and associated assets

---
+++ tekara/Views/Components/ChapterHintBubble.swift
@@ -0,0 +1,18 @@
+//
+//  ChapterHintBubble.swift
+//  tekara
+//
+//  Created by DIMAS DAFFA ERNANDA on 23/07/26.
+//
+
+import SwiftUI
+
+struct ChapterHintBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChapterHintBubble()
+}
