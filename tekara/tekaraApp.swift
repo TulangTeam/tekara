60fbb39ef06248b347ecf2f80c89676d26f6aef4
dimasdaffa <dimas.de2004@gmail.com>
2026-07-30 23:29:22 +0700

    feat: add custom font registration and integrate DebugSwift for debugging

--- tekara/tekaraApp.swift
+++ tekara/tekaraApp.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import CoreText
+#if DEBUG
+import DebugSwift
+#endif
 
 @main
 struct tekaraApp: App {
@@ -19,6 +23,36 @@ struct tekaraApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    #if DEBUG
+    private let debugSwift = DebugSwift()
+    #endif
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        registerCustomFonts()
+        #if DEBUG
+        debugSwift.setup()
+        // debugSwift.show()
+        #endif
+        return true
+    }
+
+    private func registerCustomFonts() {
+        guard let fontURL = Bundle.main.url(forResource: "Baloo2-VariableFont_wght", withExtension: "ttf") else {
+            print("[Font] Baloo2-VariableFont_wght.ttf not found in bundle")
+            return
+        }
+        var error: Unmanaged<CFError>?
+        if !CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &error) {
+            if let cfError = error?.takeRetainedValue() {
+                print("[Font] Failed to register Baloo 2: \(cfError)")
+            }
+        }
+    }
+
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return .landscape
     }
